24a18063a45d2a11255e380a46ecfc3793c914cf
Leonardo Anjos <leodrums11@gmail.com>
2021-08-28 18:51:38 -0300

    config env

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-const express = require('express');
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
 
+const express = require('express');
 class AppController {
   constructor() {
     this.express = express();
