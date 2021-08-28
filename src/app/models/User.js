0963d5fa65175d00d357cb097a33f4be9831955d
Leonardo Anjos <leodrums11@gmail.com>
2021-08-28 19:18:58 -0300

    refactor user model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user',  {
+  const User = sequelize.define('User',  {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password_hash: DataTypes.STRING
   })
 
-  return user;
+  return User;
 };
