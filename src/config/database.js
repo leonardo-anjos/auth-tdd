867578060bf25bde022def8c2c86a1dd9265ec8d
Leonardo Anjos <leodrums11@gmail.com>
2021-08-28 17:53:44 -0300

    orm sequelize config

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'querywork',
+  password: 'VgZ5unaViW',
+  database: 'querywork',
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
